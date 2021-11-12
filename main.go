f6bbd0249f0b310dd0c78fe17cebb858902e31ee
Matt White <matthew.white.john@gmail.com>
2021-11-11 23:59:53 -0500

    Break out subcommands into separate functions
    "main()" needed some cleanup!
    
    Rather than returning the error and checking it in main, return
    the exit status based on the error (0 if successful). Makes things
    a tad bit cleaner.
    
    "exitStatus" not necessary, primarily for explicitly denoting ints
    as being associated with exit statuses specifically.
    
    This model isn't necessary by any means if just exiting from main,
    but main was too big, and trying to avoid exiting from anywhere
    else _but_ main...

--- main.go
+++ main.go
@@ -10,96 +10,129 @@ import (
 )
 
 const (
-	argError = 1 << iota
+	success  exitStatus = 0
+	argError exitStatus = 1 << iota
 	spotifyError
 	dbError
 	homeError
 )
 
+type exitStatus int
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Fprintln(os.Stderr, "Must specify command.")
-		os.Exit(argError)
+		os.Exit(int(argError))
 	}
 
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
-		os.Exit(homeError)
+		os.Exit(int(homeError))
 	}
 
 	db, err := db.New(homeDir + "/.spotty")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
-		os.Exit(dbError)
+		os.Exit(int(dbError))
 	}
 	defer db.Close()
 
 	if err = db.Init(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
-		os.Exit(dbError)
+		os.Exit(int(dbError))
 	}
 
 	switch os.Args[1] {
 
 	case "pp":
-		err := spotify.PlayPause()
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(spotifyError)
-		}
+		os.Exit(int(playPause()))
 
 	case "next":
-		err := spotify.Next()
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(spotifyError)
-		}
+		os.Exit(int(next()))
 
 	case "save":
-		curr, err := spotify.Current()
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(spotifyError)
-		}
-
-		err = db.Insert(curr)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(dbError)
-		}
+		os.Exit(int(save(db)))
 
 	case "play":
-		if len(os.Args) <= 2 {
-			fmt.Fprintln(os.Stderr, "Must specify search.")
-			os.Exit(argError)
-		}
-
-		track, err := db.Get(strings.Join(os.Args[2:], " "))
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(dbError)
-		}
-
-		err = spotify.Play(track)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(spotifyError)
-		}
+		os.Exit(int(play(db)))
 
 	case "list":
-		tracks, err := db.GetAll()
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(dbError)
-		}
-
-		for _, t := range tracks {
-			fmt.Println(t.Data)
-		}
+		os.Exit(int(list(db)))
 
 	default:
 		fmt.Fprintln(os.Stderr, "Invalid command.")
-		os.Exit(argError)
+		os.Exit(int(argError))
 	}
+}
+
+func playPause() exitStatus {
+	err := spotify.PlayPause()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return spotifyError
+	}
+
+	return success
+}
+
+func next() exitStatus {
+	err := spotify.Next()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return spotifyError
+	}
+
+	return success
+}
+
+func save(db db.DB) exitStatus {
+	curr, err := spotify.Current()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return spotifyError
+	}
+
+	err = db.Insert(curr)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return dbError
+	}
+
+	return success
+}
+
+func list(db db.DB) exitStatus {
+	tracks, err := db.GetAll()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return dbError
+	}
+
+	for _, t := range tracks {
+		fmt.Println(t.Data)
+	}
+
+	return success
+}
+
+func play(db db.DB) exitStatus {
+	if len(os.Args) <= 2 {
+		fmt.Fprintln(os.Stderr, "Must specify search.")
+		return argError
+	}
+
+	track, err := db.Get(strings.Join(os.Args[2:], " "))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return dbError
+	}
+
+	err = spotify.Play(track)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return spotifyError
+	}
+
+	return success
 }
