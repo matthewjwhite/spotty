50a0d350606cb1d28d14ddda0dbd5909424907cb
Matt White <matthew.white.john@gmail.com>
2021-11-12 00:22:10 -0500

    Minor comment cleanup

--- spotify/osa.go
+++ spotify/osa.go
@@ -6,35 +6,36 @@ import (
 	"github.com/andybrewer/mack"
 )
 
-// Play plays a track in Spotify via OSA.
+// Play plays a track given a track URI.
 func Play(track Track) (err error) {
 	_, err = mack.Tell("Spotify", "play track \""+track.URI+"\"")
 
 	return
 }
 
-// Next plays the next track in Spotify via OSA.
-// If executed in the context of an album/playlist that the user explicitly
-// started from, Spotify will play the next song (next song in sequence,
-// or shuffled). Else, will be random. In short, the context is not preserved in the DB.
+// Next plays the next track.
+// If executed in the context of an album/playlist that the user started,
+// Spotify will play the next song (next song in sequence, or shuffled).
+// Else, will be random.
 func Next() (err error) {
 	_, err = mack.Tell("Spotify", "play next track")
 
 	return
 }
 
-// PlayPause toggles play/pause in Spotify via OSA.
+// PlayPause toggles play/pause.
 func PlayPause() (err error) {
 	_, err = mack.Tell("Spotify", "playpause")
 
 	return
 }
 
-// Current returns the current track playing/paused in Spotify via OSA.
+// Current returns the current track playing/paused.
 func Current() (Track, error) {
 	ret, err := mack.Tell("Spotify",
 		"get {spotify url, artist, name, album} of current track")
 
+	// Split URI from everything else.
 	parts := strings.SplitN(ret, ", ", 2)
 
 	if err != nil {
